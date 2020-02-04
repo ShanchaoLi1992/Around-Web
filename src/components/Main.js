e9834a5686f0a5fed7d2ba80c23f8e6cbebffc0d
Shanchao Li <shanchao.li@care.com>
2020-02-04 15:04:36 -0800

    Push to git

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Register} from './Register.js';
+import {Redirect, Route, Switch} from 'react-router-dom';
+import {Login} from "./Login";
+import Home from "./Home";
+
+class Main extends Component {
+    getLogin = () => {
+        return this.props.isLoggedIn ? <Redirect to = "/home"/>
+        : <Login handleLoginSucceed = {this.props.handleLoginSucceed} />
+    }
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <Switch>
+                    <Route path = "/register" component = {Register} />
+                    <Route path = "/login" render = { this.getLogin } />
+                    <Route path = "/home" render={this.getHome} />
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Main;
